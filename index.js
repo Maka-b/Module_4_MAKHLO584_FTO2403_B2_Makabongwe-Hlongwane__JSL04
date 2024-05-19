45f56b39b4a4e4e056a9a4840f2862b34d0d253a
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-05-19 12:07:46 +0200

    Updated

--- index.js
+++ index.js
@@ -5,14 +5,21 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+/*if (!userPreference){
+    userPreference = 'Regular'
+}*/
+userPreference ? 'Sugar-free' : 'Regular'
+console.log(userPreference);
+
+
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+const result = cansLeft < 5  ? "Time to restock":"We're stocked!"
+console.log(result);
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,7 +27,8 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+const condition = heartRate < 100 ? "Boost needed!" : "Energy levels are high!"
+console.log(condition);
 
 
 // #### Challenge 4: Temperature Suitability
@@ -28,7 +36,8 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+const tempState = currentTemp < 5 ? "Chilled to perfection!" : "Needs a cooler!"
+console.log(tempState);
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -38,6 +47,8 @@ console.log(``);
 
 // Define the current hour in 24-hour format
 let currentHour = 22; 
+const choice = 7 < currentHour < 24 ?  "Unleash the beast!":"Better stick to water."
+console.log(choice) 
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
